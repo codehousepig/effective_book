bdea953bfb74b8bbcdcb2a93391ccc0319fd589a
정천우 <codehouse95@gmail.com>
2023-03-13 23:47:52 +0900

    init

---
+++ src/main/java/hp/effective/effective_book/EffectiveBookApplication.java
@@ -0,0 +1,13 @@
+package hp.effective.effective_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hp/effective/effective_book/EffectiveBookApplicationTests.java
@@ -0,0 +1,13 @@
+package hp.effective.effective_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

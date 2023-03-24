5494e73d1f98aec5ab73a7ae11dcfd7ef0e8476d
정천우 <codehouse95@gmail.com>
2023-03-25 00:55:56 +0900

    예제 1.5 두 요소를 지닌 목록을 위한 테스트 케이스

--- src/test/java/hp/effective/effective_book/ch01/PlanningPokerTest.java
+++ src/test/java/hp/effective/effective_book/ch01/PlanningPokerTest.java
@@ -32,4 +32,32 @@ class PlanningPokerTest {
 			new PlanningPoker().identifyExtremes(list);
 		}).isInstanceOf(IllegalArgumentException.class);
 	}
+
+	@Test
+	void twoEstimates() {
+		List<Estimate> list = Arrays.asList( // 두 개의 추정을 지닌 목록을 정의한다.
+			new Estimate("Mauricio", 10),
+			new Estimate("Frank", 5)
+		);
+		// 2. 테스트 대상 메서드인 identifyExtremes
+		List<String> devs = new PlanningPoker().identifyExtremes(list);
+
+		// 3. 메서드가 두 개발자를 반환하는지 단언한다.
+		assertThat(devs).containsExactlyInAnyOrder("Mauricio", "Frank");
+	}
+
+	@Test
+	void manyEstimates() {
+		List<Estimate> list = Arrays.asList( // 4. 이제 세 개발자의 추정을 지닌 목록을 정의한다.
+			new Estimate("Mauricio", 10),
+			new Estimate("Arie", 5),
+			new Estimate("Frank", 7)
+		);
+		// 5. 테스트상에서 다시 메서드를 호출한다.
+		List<String> devs = new PlanningPoker().identifyExtremes(list);
+
+		// 6. 올바른 개발자 이름인 Mauricio, Arie를 반환하는지 확인한다.
+		assertThat(devs).containsExactlyInAnyOrder("Mauricio", "Arie");
+	}
+
 }
